01fdd762fbf8b69e81ed06108a6518a9bf54b577
mic <micmor.m@gmail.com>
2021-05-17 22:59:38 -0400

    Add logic to calculate result

--- main.py
+++ main.py
@@ -24,3 +24,17 @@ operations = {
   "*": multiply,
   "/": divide,
 }
+
+num1 = int(input("What is the first number?: "))
+num2 = int(input("What is the second number?: "))
+
+# Print list of operation to user:
+for operation in operations:
+    print(operation)
+
+operation_symbol = (input("Pick an operation from the line above: "))
+
+function = operations[operation_symbol]
+result = function(num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {result}")
+
