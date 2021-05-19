d0a245bf94aa761496dcf0d2fd161c865a1a34d5
mic <micmor.m@gmail.com>
2021-05-18 23:55:58 -0400

    Add call of the function when the user type n to reset the total in memory.

--- main.py
+++ main.py
@@ -24,24 +24,30 @@ operations = {
   "*": multiply,
   "/": divide,
 }
-running = "y"
-is_first_loop = True
-while running == "y":
-    num1 = int(input("What is the first number?: "))
-    # Print list of operation to user:
-    for operation in operations:
-        print(operation)
-    operation_symbol = (input("Pick an operation from the line above: "))
-    function = operations[operation_symbol]
-
-    if is_first_loop:
-        num2 = int(input("What is the second number?: "))
-    else:
-        num2 = result
-
-    result = function(num1, num2)
-    print(f"{num1} {operation_symbol} {num2} = {result}")
-    running = input(f"Type 'y' to continue calculating with {result}, or type 'n' to exit: ")
-    is_first_loop = False
 
 
+def calculator():
+    running = "y"
+    is_first_loop = True
+    while running == "y":
+        num1 = int(input("What is the first number?: "))
+        # Print list of operation to user:
+        for operation in operations:
+            print(operation)
+        operation_symbol = (input("Pick an operation from the line above: "))
+        function = operations[operation_symbol]
+
+        if is_first_loop:
+            num2 = int(input("What is the second number?: "))
+        else:
+            num2 = result
+
+        result = function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {result}")
+        running = input(f"Type 'y' to continue calculating with {result}, or type 'n' to exit: ")
+        is_first_loop = False
+        if running == "n":
+            calculator()
+
+
+calculator()
